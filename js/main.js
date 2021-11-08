fcf0e9f35ef0ba3f439bc389d526760da43cf96b
lukasz ciepichal <lukaszciepichal1@gmail.com>
2021-11-08 16:16:19 +0100

    zmiana nazw parametrow

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let num1 = 2;
 let num2 = 5;
 
-function ktoraWieksza(num1, num2){
-    if (num1 > num2)
-    return (num1)
+function ktoraWieksza(number1, number2){
+    if (number1 > number2)
+    return (number1)
 }
 console.log(ktoraWieksza(num2, num1))
