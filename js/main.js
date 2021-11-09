68f277d64eb70b58d0624723b03c06caa6b6f34d
lukasz ciepichal <lukaszciepichal1@gmail.com>
2021-11-09 12:39:34 +0100

    dodanie warunku

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ let num1 = 2;
 let num2 = 5;
 
 function ktoraWieksza(number1, number2){
-    if (number1 > number2)
-    return (number1)
+    if (number1 > number2) {
+        return (number1);
+    } else if (number1 < number2) {
+        return number2;
+    }
+    
 }
 console.log(ktoraWieksza(num2, num1))
